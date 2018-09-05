2145dd2603c26cc40ace0c005c2363969cce77a2
Pramod <pramod@coupang.com>
2018-09-05 13:27:05 -0700

    Code page changes.

--- java/framework/CodeGenerator.java
+++ java/framework/CodeGenerator.java
@@ -1,8 +1,9 @@
 package framework;
 
-import com.sun.tools.javac.jvm.Code;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,9 +11,23 @@ import java.util.Properties;
 
 public class CodeGenerator {
 
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Prop File: ");
+            String propFile = br.readLine();
+            Properties properties = new Properties();
+            String props = CodeConfiguration.PROPERTIES_PATH.getValue() + propFile;
+            properties.load(new FileInputStream(props));
+            new CodeGenerator().generateHtml(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void generateHtml(Properties properties) throws Exception {
         Path path = Paths.get(getTemplateFile());
-        String destination = getDestinationPath(getFilePath(properties.getProperty(TemplateKeys.title.getValue())));
+        String filePrefix = getFilePath(properties.getProperty(TemplateKeys.title.getValue()));
+        String destination = getDestinationPath(filePrefix);
 
         String content = new String(Files.readAllBytes(path));
         content = content.replaceAll(getHandler(TemplateKeys.title.getValue()),
@@ -25,12 +40,21 @@ public class CodeGenerator {
 
         //Question
         content = content.replaceAll(getHandler(TemplateKeys.question.getValue()),
-                readFile(CodeConfiguration.QUESTION_BASE_PATH.getValue() + destination
-                        + CodeConfiguration.QUESTION_FILE_EXTENSION)
+                readFile(CodeConfiguration.QUESTION_BASE_PATH.getValue() + filePrefix
+                        + CodeConfiguration.QUESTION_FILE_EXTENSION.getValue())
         );
 
+        //Solution
+        String solutionPath = CodeConfiguration.SOLUTION_PATH.getValue()
+                + properties.getProperty(TemplateKeys.solution.getValue());
         content = content.replaceAll(getHandler(TemplateKeys.solution.getValue()),
-                properties.getProperty(TemplateKeys.solution.getValue()));
+                readFile(solutionPath)
+        );
+
+        //Disqus
+        content = content.replaceAll(getHandler(TemplateKeys.disqus.getValue()),
+                filePrefix
+        );
 
         Files.write(Paths.get(destination), content.getBytes());
     }
@@ -40,7 +64,8 @@ public class CodeGenerator {
     }
 
     private String getDestinationPath(String destinationFileName) {
-        return CodeConfiguration.DESTINATION_BASE_PATH.getValue() + destinationFileName;
+        return CodeConfiguration.DESTINATION_BASE_PATH.getValue()
+                + destinationFileName + CodeConfiguration.HTML_EXTENSION.getValue();
     }
 
     private String readFile(String file) throws IOException {
@@ -63,7 +88,8 @@ public class CodeGenerator {
         title("page.title"),
         header("page.header"),
         question("page.question"),
-        solution("page.solution");
+        solution("page.solution"),
+        disqus("page.disqus.identifier");
 
         String value;
 
@@ -81,10 +107,11 @@ public class CodeGenerator {
         DESTINATION_BASE_PATH(BASE_PATH.getValue() + "p/"),
         TEMPLATE_FILE(BASE_PATH.getValue() + "public/templates/codetemplate.html"),
         HEADER_FILE(BASE_PATH.getValue() + "header.html"),
-        QUESTION_BASE_PATH(BASE_PATH.getValue() + "questions/"),
         QUESTION_FILE_EXTENSION(".question.html"),
         SOLUTION_PATH(BASE_PATH.getValue() + "java/code/"),
-        PROPERTIES_PATH(SOLUTION_PATH.getValue() + "properties/");
+        QUESTION_BASE_PATH(SOLUTION_PATH.getValue() + "questions/"),
+        PROPERTIES_PATH(SOLUTION_PATH.getValue() + "properties/"),
+        HTML_EXTENSION(".html");
 
         String value;
 
@@ -94,19 +121,6 @@ public class CodeGenerator {
 
         public String getValue() {
             return value;
-        }
-    }
-
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Prop File: ");
-            String propFile = br.readLine();
-            Properties properties = new Properties();
-            String props = CodeConfiguration.PROPERTIES_PATH.getValue() + propFile;
-            properties.load(new FileInputStream(props));
-            new CodeGenerator().generateHtml(properties);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
