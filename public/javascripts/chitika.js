d0003071abb3fadd336422e6809e8530c1a77179
pramodnanduri <pramodnanduri@ymail.com>
2018-09-20 12:45:35 -0700

    LeetCode - Containter with Maximum height.

--- public/javascripts/chitika.js
+++ public/javascripts/chitika.js
@@ -3,5 +3,5 @@
         var unit = {"calltype":"async[2]","publisher":"manigopal","width":728,"height":90,"sid":"Chitika Default"};
         var placement_id = window.CHITIKA.units.length;
         window.CHITIKA.units.push(unit);
-        document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+        $('#adSpot').html('<div id="chitikaAdBlock-' + placement_id + '"></div>');
     }())
