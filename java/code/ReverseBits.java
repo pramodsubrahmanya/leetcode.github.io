c6e1cce16093455cfa47d18db9756cd80110eb5a
pramodnanduri <pramodnanduri@ymail.com>
2018-11-18 19:05:18 -0800

    LeetCode - Solutions

--- java/code/ReverseBits.java
+++ java/code/ReverseBits.java
@@ -1,28 +1,31 @@
 package code;
 
+/**
+ * This is an awesome conceptual question.
+ * Input integer n , is depicted as bit string of 32 bits.
+ * Each bit of the input is 'OR' with (31-i) times of the
+ * input position.
+ * Eg: Input: 0001
+ * So answer would be : sum = sum | (1 << 31-i))
+ * i is the current position.
+ */
 public class ReverseBits {
 
     public int reverseBits(int n) {
-        System.out.println(n);
-        long k = n;
-        long res = 0;
-        int pow = 31;
-        StringBuilder result = new StringBuilder();
-        while (k != 0) {
-            long rem = k % 2;
-            res += rem * Math.pow(2, pow);
-//            result.insert(0, rem);
-            k = k / 2;
-            pow--;
+        int mask = 1;
+        int sum = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                sum = sum | (1 << (31 - i));
+            }
+            mask <<= 1;
         }
-
-        return (int) res;
+        return sum;
     }
 
     public static void main(String[] args) {
         String n = "2147483649";
-//        int x = Integer.parseUnsignedInt("2147483648");
-        //System.out.println();
+        n = "1";
         System.out.println(new ReverseBits().reverseBits(Integer.parseInt(n)));
     }
 
